9ad5a1833298edb941952db06a4e874262650103
Tiogle <toontowninfinitehelp@yahoo.com>
2017-04-14 16:10:57 -0500

    Music Bot Input

---
+++ Music.js
@@ -0,0 +1,35 @@
+const yt = require('ytdl-core');
+
+client.on('message', message => {
+  var args = message.content.split (" ");
+  if (message.content.startsWith('++play')) {
+    const voiceChannel = message.member.voiceChannel;
+    if (!voiceChannel) {
+      return message.reply("Please be in a voice channel first!");
+    }
+    voiceChannel.join()
+     .then(connection => {
+        yt.getInfo(args[1], (err, info) => {
+          if (err) {
+            connection.disconnect();
+            return message.reply("Couldn't find that video");
+          }
+        const name = info.title;
+        const stream = yt.downloadFromInfo(info, { filter: "audioonly" });
+        const dispatcher = connection.playStream(stream)
+          .once("start", () => {
+            message.reply(`Now playing ${name}`);
+          })
+          .once("end", () => {
+            connection.disconnect();
+          });
+      });
+    });
+  }
+  if (message.content.startsWith('++stop')) {
+    if (message.guild && message.guild.voiceConnection) {
+      message.guild.voiceConnection.disconnect();
+      message.reply("Nothing is now playing");
+    }
+  }
+});
